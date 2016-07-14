42cc78c31d34ca88961958a04153e75922d8b022
Jorge <Jorge>
2016-07-14 00:42:23 -0500

    Server con IP y puerto tomados del entorno Lab03

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 //http
 var http = require("http");
+//Obteniendo informacion del enterno de ejecucion con respecto a la IP y al puerto que debemos usar en nuestro server
+var PORT = process.env.PORT || 3000;
+var IP =process.env.IP || '127.0.0.1';
+
 //crear un servidor basico
 var server = http.createServer(function(req, res){
     //armar la respuesta http
@@ -10,10 +14,14 @@ var server = http.createServer(function(req, res){
     });
     //enviamos la respuesta
     res.write("Hola Mundo");
+    res.write("Javo");
     //Cerrar la conexion
     res.end();
 });
 //poner a trabajar el server
 server.listen(3000, '127.0.0.1', function(){
     console.log("> Server listening @http://localhost:3000 ...");
+    server.listen(PORT,IP,function () {
+console.log(`> Server listening @http://${IP}:${PORT} ...`)
+    });
 });
